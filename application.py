46dca04f3eb3638f9b0c5c60f63ad4ae9778144f
unknown <minhquanym@gmail.com>
2020-03-10 18:56:13 +0700

    Complete login page

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, render_template, request
 from models import *
+from operator import and_
 
 
 app = Flask(__name__)
@@ -9,21 +10,29 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
-curUser = -1
+curUserId = -1
 @app.route("/")
 def index():
-    if curUser != -1:
-        return render_template("welcome.html")
-    return render_template("index.html")
+    if curUserId != -1:
+        return render_template("welcome.html", curUserId)
+    return render_template("index.html", message="")
 
-@app.route("/login")
+@app.route("/login", methods = ["POST"])
 def login():
-    if curUser != -1:
-        return render_template("welcome.html")
+    # Login to your account
+    if curUserId != -1:
+        return render_template("welcome.html", curUserId)
+    username = request.form.get("username")
+    password = request.form.get("password")
+    
+    user = User.query.filter(and_(User.username == username, User.password == password)).first()
+    if not user:
+        return render_template("index.html", message="Your username or password is wrong")
+
     return render_template("login.html")
 
 @app.route("/register")
 def register():
-    if curUser != -1:
-        return render_template("welcome.html")
+    if curUserId != -1:
+        return render_template("welcome.html", curUserId)
     return render_template("register.html")
