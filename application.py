98aed2d03453d6710c08d719f892fedcceaf6cbe
unknown <minhquanym@gmail.com>
2020-03-11 16:08:09 +0700

    Show list of books

--- application.py
+++ application.py
@@ -1,16 +1,20 @@
 import os
 
 from flask import Flask, render_template, request
+# from flask_login import LoginManager
 from models import *
-from operator import and_
-
+from operator import and_, or_
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
+
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 curUserId = -1
+
 @app.route("/")
 def index():
     if curUserId != -1:
@@ -63,3 +67,15 @@ def newuser():
     db.session.commit()
     return render_template("index.html", message="You registered completely. Lets login to review books")
     
+@app.route('/search', methods = ["POST", "GET"])
+def search():
+    text = request.form.get("search")
+    books = Book.query.limit(30).all()
+    return render_template("welcome.html", books=books)
+
+@app.route("/book/<int:book_id>")
+def book(book_id):
+    book = Book.query.get(book_id)
+    if book is None:
+        return render_template("welcome.html")
+    return render_template("book.html", book=book)
