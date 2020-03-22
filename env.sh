2d2a83aabcb7122077c43b105257c1335bfa7436
Minh Quan <minhquanym@gmail.com>
2020-03-22 09:10:52 +0700

    deploy to heroku

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
-SET FLASK_APP=application.py
-SET FLASK_DEBUG=1
-SET DATABASE_URL=postgres://edrftvrfwnuhig:b1105643aa5377f1391258f82e7dda43f494eb2e8a966677d513c692be6dcdfc@ec2-34-197-212-240.compute-1.amazonaws.com:5432/d1tkk3mik8afu3
+export FLASK_APP=application.py
+export FLASK_DEBUG=1
+export DATABASE_URL=postgres://edrftvrfwnuhig:b1105643aa5377f1391258f82e7dda43f494eb2e8a966677d513c692be6dcdfc@ec2-34-197-212-240.compute-1.amazonaws.com:5432/d1tkk3mik8afu3
